e1318105632cfbad0053b111008c433dfffc0714
Marqvardi <marquinhos.vardi@gmail.com>
2020-04-21 23:08:57 +0100

    Add project files.

---
+++ Vega/Migrations/20200421203843_SeedDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Makes (name) values ('Make1')");
+            migrationBuilder.Sql("insert into Makes (name) values ('Make2')");
+            migrationBuilder.Sql("insert into Makes (name) values ('Make3')");
+            migrationBuilder.Sql("insert into Makes (name) values ('Make4')");
+
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make1-ModelA', 1)");
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make1-ModelB', 1)");
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make1-ModelC', 1)");
+
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make2-ModelA', 2)");
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make2-ModelB', 2)");
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make2-ModelC', 2)");
+
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make3-ModelA', 3)");
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make3-ModelB', 3)");
+            migrationBuilder.Sql("insert into Models (name, makeId) values ('Make3-ModelC', 3)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Makes");            
+        }
+    }
+}
